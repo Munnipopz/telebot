e4df9c5dabd0d72e3c4e6f5cf61a14df3ab7a3d2
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-01-22 16:24:13 +0530

    Update _help.py

--- telebot/plugins/_help.py
+++ telebot/plugins/_help.py
@@ -21,10 +21,10 @@ from telebot.telebotConfig import Config
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TeleBot User"
 CMD_HNDLR = Config.CMD_HNDLR
-CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "⚡")
+CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "🔹")
 
 if CMD_HNDLR is None:
-    CMD_HNDLR = "."
+    CMD_HNDLR = "!"
 
 
 @telebot.on(admin_cmd(pattern="help ?(.*)"))
